7bdee83dc5069efc11f205acb50075b9b3d40a13
nkrzykal <01168987@pw.edu.pl>
2023-12-17 00:15:22 +0100

    Security naprawione by Mrozek

--- src/main/java/SalonSamochodowy_BADA_projekt/SalonSamochodowyAplikacja/AppController.java
+++ src/main/java/SalonSamochodowy_BADA_projekt/SalonSamochodowyAplikacja/AppController.java
@@ -1,12 +1,15 @@
 package SalonSamochodowy_BADA_projekt.SalonSamochodowyAplikacja;
 
-import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+
 @Configuration
 public class AppController implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
