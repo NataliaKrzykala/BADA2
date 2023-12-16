07be4449cb2fb4c626609e4d8b288ac8218cc024
nkrzykal <01168987@pw.edu.pl>
2023-12-16 19:42:34 +0100

    Baza z przykladow do konca prezentacji 3

---
+++ src/main/java/SalonSamochodowy_BADA_projekt/SalonSamochodowyAplikacja/SalonSamochodowyAplikacjaApplication.java
@@ -0,0 +1,13 @@
+package SalonSamochodowy_BADA_projekt.SalonSamochodowyAplikacja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonSamochodowyAplikacjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonSamochodowyAplikacjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SalonSamochodowy_BADA_projekt/SalonSamochodowyAplikacja/SalonSamochodowyAplikacjaApplicationTests.java
@@ -0,0 +1,13 @@
+package SalonSamochodowy_BADA_projekt.SalonSamochodowyAplikacja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonSamochodowyAplikacjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
